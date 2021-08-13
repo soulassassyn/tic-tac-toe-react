78b00a231a0644775e59c02f1f3f2f59f9dce506
soulassassyn <53498704+soulassassyn@users.noreply.github.com>
2021-08-13 16:55:08 -0600

    adding project structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <title>Tic Tac Toe</title>
+    <style></style>
+    <body>Click to play Tic Tac Toe!</body>
+    </div>
+  );
+}
+
+export default App;
